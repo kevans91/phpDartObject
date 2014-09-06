978938d08a48e19718bf3debf5c9f4a803605217
Kyle Evans <kevans91@ksu.edu>
2014-09-06 05:04:31 -0400

    Filter out __private vars in DartResponse->toString

--- DartResponse.class.php
+++ DartResponse.class.php
@@ -1,6 +1,16 @@
 <?php
 class DartResponse {
 	public function toString() {	
-		return json_encode($this);
+		$vars = get_object_vars($this);
+		
+			// Filter out __variables
+		$response = new stdClass();
+		foreach($vars as $var => $val) {
+			if(strpos($var, '__') === 0) continue;
+
+			$response->$var = $val;
+		}
+
+		return json_encode($response);
 	}
 }
